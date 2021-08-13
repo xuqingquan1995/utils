b7375105486710d2b2cad46ef5b64be5829ccec7
许清泉 <xuqingquan1995@gmail.com>
2021-08-13 16:58:25 +0800

    添加示例代码

---
+++ demo/src/main/java/top/xuqingquan/utils/demo/MainActivity.kt
@@ -0,0 +1,25 @@
+package top.xuqingquan.utils.demo
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import top.xuqingquan.utils.Timber
+import top.xuqingquan.utils.noFastClick
+import top.xuqingquan.utils.startActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<View>(R.id.btn)
+        val btn1 = findViewById<View>(R.id.btn1)
+        btn.noFastClick(5000) {
+            Timber.d("btnbtnbtnbtnbtn")
+            startActivity<TestActivity>()
+        }
+        btn1.noFastClick {
+            Timber.d("btn1btn1btn1btn1")
+        }
+    }
+}
