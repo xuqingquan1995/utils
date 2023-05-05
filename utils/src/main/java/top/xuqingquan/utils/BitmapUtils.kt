44fb523753d14433060a8ce24005bfad9ca3390d
许清泉 <xuqingquan1995@gmail.com>
2023-05-05 10:07:45 +0800

    优化bitmap保存成文件的逻辑

--- utils/src/main/java/top/xuqingquan/utils/BitmapUtils.kt
+++ utils/src/main/java/top/xuqingquan/utils/BitmapUtils.kt
@@ -43,27 +43,51 @@ fun view2Bitmap(view: View?): Bitmap? {
 
 /**
  * 将bitmap保存成图片
+ * @param context 上下文
+ * @param bmp 要保存的bitmap
+ * @param outDir 要保存的路径
+ * @param ext 要保存的文件后缀
+ * @param quality 压缩率
  */
-fun bitmap2File(context: Context?, bmp: Bitmap): File? {
+fun bitmap2File(
+    context: Context?, bmp: Bitmap, outDir: File? = null, ext: String = "png", quality: Int = 100
+): File? {
     if (context == null) {
         return null
     }
-    // 判断是否可以对SDcard进行操作
-    val sdCardDir = context.getExternalFilesDir("images")!!.absolutePath
-    //目录转化成文件夹
-    val dirFile = File(sdCardDir)
-    //如果不存在，那就建立这个文件夹
-    if (!dirFile.exists()) {
-        dirFile.mkdirs()
+    val file: File
+    if (outDir == null) {
+        // 判断是否可以对SDcard进行操作
+        val sdCardDir = context.getExternalFilesDir("images")!!.absolutePath
+        //目录转化成文件夹
+        val dirFile = File(sdCardDir)
+        //如果不存在，那就建立这个文件夹
+        if (!dirFile.exists()) {
+            dirFile.mkdirs()
+        }
+        // 在SDcard的目录下创建图片文,以当前时间为其命名，注意文件后缀，若生成为JPEG则为.jpg,若为PNG则为.png
+        file = File(sdCardDir, "${System.currentTimeMillis()}.${ext}")
+    } else if (outDir.absolutePath.endsWith("/")) {//是一个文件夹地址
+        if (!outDir.exists()) {
+            outDir.mkdirs()
+        }
+        file = File(outDir, "${System.currentTimeMillis()}.${ext}")
+    } else {
+        if (outDir.parentFile?.exists() == false) {
+            outDir.parentFile?.mkdirs()
+        }
+        file = outDir
     }
-    // 在SDcard的目录下创建图片文,以当前时间为其命名，注意文件后缀，若生成为JPEG则为.jpg,若为PNG则为.png
-    val file = File(sdCardDir, System.currentTimeMillis().toString() + ".png")
     var out: FileOutputStream? = null
     try {
         out = FileOutputStream(file)
         //将bitmap（数值100表示不压缩）存储到out输出流中去，图片格式为JPEG
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, out)
-        //bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
+        val format = if (ext == "png"){
+            Bitmap.CompressFormat.PNG
+        }else{
+            Bitmap.CompressFormat.JPEG
+        }
+        bmp.compress(format, quality, out)
     } catch (e: Throwable) {
         e.printStackTrace()
     }
