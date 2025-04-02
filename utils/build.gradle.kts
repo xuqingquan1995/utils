9d6388ea3949e4be1b6ba467e93aaf8e15b670b6
许清泉 <xuqingquan1995@gmail.com>
2025-04-02 19:53:46 +0800

    更新部分依赖，更新版本

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -47,9 +47,9 @@ dependencies {
     implementation("androidx.exifinterface:exifinterface:1.3.7")
     //test
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:rules:1.6.1")
 }
 
 afterEvaluate {
@@ -59,7 +59,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "top.xuqingquan"
                 artifactId = "utils"
-                version = "3.2.3"
+                version = "3.2.5"
             }
         }
     }
